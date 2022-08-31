057313e211e154597e70528b309dd5bdcee6e231
dev-gaikwad <dev-gaikwad@users.noreply.github.com>
2022-08-31 21:55:07 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ import { MoviesProvider } from './context/MoviesContext';
 
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
+import AboutPage from './pages/AboutPage';
 
 function App() {
-  const appName = 'Movie Fanatics';
-
   return (
-    <Router>
-      <MoviesProvider>
-        <Header appName={appName} />
-        <Main />
-      </MoviesProvider>
-    </Router>
+    <MoviesProvider>
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <Header />
+                <Main />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route path='/about' element={<AboutPage />}></Route>
+        </Routes>
+      </Router>
+    </MoviesProvider>
   );
 }
 
--- src/context/MoviesContext.js
+++ src/context/MoviesContext.js
@@ -3,6 +3,8 @@ import { createContext, useState, useEffect } from 'react';
 const MoviesContext = createContext();
 export const MoviesProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
+
+  const appName = 'Movie Fanatics';
   const apiKey = 'b27b18620de5a2b789d6d0b01d2c2e8a';
   const discover_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=1`;
   const search_url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query="`;
@@ -21,11 +23,13 @@ export const MoviesProvider = ({ children }) => {
     if (searchTerm !== '') {
       const searchQuery = search_url + searchTerm + `"'`;
       getMoviesFromAPI(searchQuery);
-    }
+    } else window.location.reload();
   }
+
   return (
     <MoviesContext.Provider
       value={{
+        appName,
         movies,
         searchQueryInAPI,
       }}
