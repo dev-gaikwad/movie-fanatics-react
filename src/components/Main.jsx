c186c678af2ca6388602a8d500ed23de4057658d
dev-gaikwad <dev-gaikwad@users.noreply.github.com>
2022-09-13 23:06:33 +0530

    style: added y-scrolling tiles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,42 @@ import ContentContext from '../context/ContentContext';
 import ContentTile from './ContentTile';
 
 function Main() {
-  const { contentList } = useContext(ContentContext);
+  const { popularList, trendingMovieList, trendingTVList, searchList } =
+    useContext(ContentContext);
+  // console.log(contentList);
 
   return (
-    <main className='main'>
-      {contentList.map((content) => (
-        <ContentTile key={content.id} content={content} />
-      ))}
-    </main>
+    <>
+      {searchList === true ? (
+        <main className='main'>
+          <h3>Search Results</h3>
+          {searchList.map((content) => (
+            <ContentTile key={content.id} content={content} />
+          ))}
+        </main>
+      ) : (
+        <main className='main'>
+          <div className='category discover'>
+            <h3>Discover</h3>
+            {popularList.map((content) => (
+              <ContentTile key={content.id} content={content} />
+            ))}
+          </div>
+          <div className='category trending-movies'>
+            <h3>Trending Movies</h3>
+            {trendingMovieList.map((content) => (
+              <ContentTile key={content.id} content={content} />
+            ))}
+          </div>
+          <div className='category trending-movies'>
+            <h3>Trending TV</h3>
+            {trendingTVList.map((content) => (
+              <ContentTile key={content.id} content={content} />
+            ))}
+          </div>
+        </main>
+      )}
+    </>
   );
 }
 
